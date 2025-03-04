f0cc8b7bcc9232a472674caac04adf2acf903587
lucasdevads <lucasadsmorais@gmail.com>
2025-03-04 15:38:44 -0300

    add exer

---
+++ fundamentos/11-tupla.py
@@ -0,0 +1,18 @@
+filmeTupla = ("Os simpsons", "DeadPool",
+"O mascara", "Madagascar", "Os sem Floresta")
+print(type(filmeTupla))
+
+# Busca os primeiro itens da lista
+print(filmeTupla[:2])
+
+# Buscar o ultimo item da tupla
+print(filmeTupla[-1])
+
+# Buscar ate uma determinaa posição
+print(filmeTupla[:3])
+
+# Buscar de uma posição em diante
+print(filmeTupla[2:])
+
+# recuperando o item pelo o nome
+print(filmeTupla.index("DeadPool"))
---
+++ fundamentos/12-set.py
@@ -0,0 +1,4 @@
+filmeSet = {"Os simpsons", "DeadPool",
+"O mascara", "Madagascar", "Os sem Floresta"}
+
+print(type(filmeSet))
