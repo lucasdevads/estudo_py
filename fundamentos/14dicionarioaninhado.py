4d2cacd6caed7555ba69b27c226a0d7f2f51a1e8
lucasdevads <lucasadsmorais@gmail.com>
2025-03-06 22:04:27 -0300

    Create 14dicionarioaninhado.py

---
+++ fundamentos/14dicionarioaninhado.py
@@ -0,0 +1,37 @@
+import pprint
+
+
+filmesDict = {
+    "os simpsons":{
+        "ano": 2013,
+        "nota": 9.0,
+        "genero": "animação"},
+
+    "Esquadrão Classe A":{
+        "ano": 2013,
+        "nota": 8.0,
+        "genero": "ação"},
+
+    "Blade":{
+        "ano": 2010,
+        "nota": 9.0,
+        "genero": "ação"},
+}
+
+pp = pprint.PrettyPrinter(depth=4)
+
+pp.pprint(filmesDict)
+
+# buscar uma informação dentro de um dicionario aninhado
+
+print(filmesDict["Blade"]["genero"])
+
+# Adicionar um novo item
+filmesDict["os simpsons"]["diretor"] = "David"
+
+print(filmesDict["os simpsons"])
+print()
+# excluir um dicionario
+del filmesDict["Blade"]
+
+pp.pprint(filmesDict)
