8589fa8f2d165a9ad2612b602f95444abab46305
lucasdevads <lucasadsmorais@gmail.com>
2025-03-07 22:00:23 -0300

    Create 15-condicao.py

---
+++ fundamentos/15-condicao.py
@@ -0,0 +1,5 @@
+# informações sobre o filme
+nome = input("Digite o nome do Filme: \n")
+anoFilme = int(input("Digite o ano de lançamento: \n"))
+avaliação = float(input("Digite a nota de avaliação do Filme: \n")) 
+
