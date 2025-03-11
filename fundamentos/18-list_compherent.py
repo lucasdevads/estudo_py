779cd69a51040e04876caf7245cfd8f26adccc73
lucasdevads <lucasadsmorais@gmail.com>
2025-03-11 07:15:46 -0300

    Update 18-list_compherent.py

--- fundamentos/18-list_compherent.py
+++ fundamentos/18-list_compherent.py
@@ -15,3 +15,20 @@ print(listFilme)
 
 filmesAssist = [filme for filme in filmsList if filme != "Frankestine"]
 print(filmesAssist)
+
+# encontrando um filme pelo nome
+
+while True:
+    buscaNome = input("Digite o nome do filme para buscar na lista ( ou sair para encerrar) \n")
+    if buscaNome.lower() == "sair":
+        print("Programa Encerrado")
+        break
+    encontraFilme = [ filme for filme in filmsList if buscaNome.lower() in filme.lower()]
+    if encontraFilme:
+        print(f"filme(s) encontrado(s) com o nome : {buscaNome}:")
+        for encontrar in encontraFilme:
+            print(encontrar)
+        else:
+            print(f"Nenhum filme foi encontrado com o nome {buscaNome}")
+
+        
