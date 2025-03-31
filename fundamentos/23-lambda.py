a8759699dd97de1dfeeac1c5da6319ec94fd3d56
lucasdevads <lucasadsmorais@gmail.com>
2025-03-31 07:40:58 -0300

    Update 23-lambda.py

--- fundamentos/23-lambda.py
+++ fundamentos/23-lambda.py
@@ -7,6 +7,8 @@ is_even = lambda x: x % 2 == 0
 # função que divide um numero por outro
 div_num = lambda x, y: x / y
 
+#função que inverte uma striung
+reverse = lambda s: s [::-1]
 
 print(power(8))
 print(power(9))
@@ -14,3 +16,4 @@ print(is_even(23))
 print(is_even(66))
 print(div_num(10,2))
 print(div_num(165,6))
+print(reverse("Lucas"))
