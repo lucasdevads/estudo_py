1fa83af9ae0f4935266422f5d0aad07825fa40cc
lucasdevads <lucasadsmorais@gmail.com>
2025-03-31 06:45:37 -0300

    Create 23-lambda.py

---
+++ fundamentos/23-lambda.py
@@ -0,0 +1,16 @@
+# função de potencia de lambda
+power = lambda num:num ** 2 
+
+# verifica se o numero e par
+is_even = lambda x: x % 2 == 0
+
+# função que divide um numero por outro
+div_num = lambda x, y: x / y
+
+
+print(power(8))
+print(power(9))
+print(is_even(23))
+print(is_even(66))
+print(div_num(10,2))
+print(div_num(165,6))
