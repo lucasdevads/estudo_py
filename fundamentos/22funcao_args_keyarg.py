d5743e8b28b6856c125d7956a047398b52df9c83
lucasdevads <lucasadsmorais@gmail.com>
2025-03-31 06:38:44 -0300

    Update 22funcao_args_keyarg.py

--- fundamentos/22funcao_args_keyarg.py
+++ fundamentos/22funcao_args_keyarg.py
@@ -1,6 +1,8 @@
 """
 args: utilizamos quando não temos certeza de quantos argumentos queremos em uma função 
 Os argumentos são pasados como uma tupla
+kwargs: Alem dos valores podemos passar tambem as respectivas chaves para cada argumento
+Os argumentos são pasado como dicionario
 """
 
 # 1 soma de números
@@ -13,3 +15,12 @@ def sum(*num):
 sum(7)
 sum(7,9)
 sum(8, 9, 34, 5, 22)
+
+# 2 - apreswentação de curso    
+def apresetacao(**data):
+    for key, value in data.items():
+        print(f"{key} - {value}")
+
+print("Lista de Cursos")
+apresetacao(nome="Python", categoria="Beck-end", level="Iniciante")
+apresetacao(nome="Django", categoria="Beck-end", level="iniciante-avançado")
